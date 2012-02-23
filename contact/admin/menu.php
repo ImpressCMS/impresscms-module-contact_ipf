48f23740a9afa667beece0f892761e3ff3c6aeaf
Simon <simon@isengard.biz>
2012-02-23 01:14:23 +0000

    Filing 1.1 final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/contact_ipf/releases/1.1%20final@23682 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ contact/admin/menu.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* Configuring the amdin side menu for the module
+*
+* @copyright	Copyright Madfish (Simon Wilkinson) 2011
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+* @package		contact
+* @version		$Id$
+*/
+
+global $icmsConfig;
+
+$module = icms::handler("icms_module")->getByDirname(basename(dirname(dirname(__FILE__))));
+
+$adminmenu[] = array(
+	"title" => _MI_CONTACT_MESSAGES,
+	"link" => "admin/message.php");
+	
+	$headermenu[] = array(
+		"title" => _CO_ICMS_GOTOMODULE,
+		"link" => ICMS_URL . "/modules/" . $module->getVar("dirname"));
+		
+	$headermenu[] = array(
+		"title" => _PREFERENCES,
+		"link" => "../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod="
+		. $module->getVar("mid"));
+	
+	$headermenu[] = array(
+		"title" => _MI_CONTACT_TEMPLATES,
+		"link" => "../../system/admin.php?fct=tplsets&op=listtpl&tplset="
+			. $icmsConfig["template_set"] . "&moddir=" . $module->getVar("dirname"));
+	
+	$headermenu[] = array(
+		"title" => _CO_ICMS_UPDATE_MODULE,
+		"link" => ICMS_URL . "/modules/system/admin.php?fct=modulesadmin&op=update&amp;module="
+		. $module->getVar("dirname"));
+
+	$headermenu[] = array(
+		"title" => _MODABOUT_ABOUT,
+		"link" => ICMS_URL . "/modules/" . $module->getVar("dirname") . "/admin/about.php");
+	
+	$headermenu[] = array(
+		"title" => _MI_CONTACT_MANUAL,
+		"link" => ICMS_URL . "/modules/" . $module->getVar("dirname") . "/manual/contact_manual.pdf");
